0815946a718835f3de09f71ac0b02ef0f50e2eb1
Dhaboav <dhaboav@gmail.com>
2024-05-22 00:34:41 +0700

    Menambah landpage

--- app/Models/Desa.php
+++ app/Models/Desa.php
@@ -7,22 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Desa extends Model {
     use HasFactory;
     protected $table = 'desa';
-    protected $fillable = [
-        'name',
-        'href',
-        'geojson_href',
-        'geojson_text',
-        'versi',
-        'type',
-        'tipe_lokasi',
-        'nama_objek',
-        'kabupaten',
-        'kecamatan',
-        'properties',
-        'workspace',
-        'subworkspace',
-        'tanggal_data',
-    ];
     public function workspace(){
         return $this -> belongsTo(workspace::class, 'workspace', 'nama');
     }
--- app/Models/Geojson.php
+++ app/Models/Geojson.php
@@ -7,22 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Geojson extends Model {
     use HasFactory;
     protected $table = 'geojson';
-    protected $fillable = [
-        'name',
-        'href',
-        'geojson_href',
-        'geojson_text',
-        'versi',
-        'type',
-        'tipe_lokasi',
-        'nama_objek',
-        'kabupaten',
-        'kecamatan',
-        'properties',
-        'workspace',
-        'subworkspace',
-        'tanggal_data',
-    ];
     public function workspace(){
         return $this -> belongsTo(workspace::class, 'workspace', 'nama');
     }
